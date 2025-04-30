f1ac460834863082f25595f33795b8edfeb3a469
JANANGA <janangathushan@gmail.com>
2025-04-30 22:11:08 +0530

    Update HallModel.js
    dd

--- backend/models/HallModel.js
+++ backend/models/HallModel.js
@@ -5,9 +5,9 @@ const mongoose = require('mongoose');
 const hallSchema = new mongoose.Schema({
   building: { type: mongoose.Schema.Types.ObjectId, ref: 'Building', required: true },
   floorNumber: { type: Number, required: true },
-  hallNumber: { type: String, required: true },     // e.g., "101", "Lab A"
-  location: { type: { x: Number, y: Number }, required: true },    // x, y coordinates on the floor map
-  hallType: { type: String, enum: ['lab', 'lecture'], required: true }   // Either 'lab' or 'lecture'
+  hallNumber: { type: String, required: true },     
+  location: { type: { x: Number, y: Number }, required: true },   
+  hallType: { type: String, enum: ['lab', 'lecture'], required: true }   
 }, { timestamps: true });
 
 const Hall = mongoose.model('Hall', hallSchema);
